c6f8da10322b9a080c1a8e051af897c3bb45cab7
Marcin <uT_3777@zsp3zamosc>
2024-11-07 09:21:59 +0100

    Dodaj pliki projektów.

---
+++ kotekk/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kotekk
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kotekk/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+
+namespace kotekk
+{
+    public partial class MainPage : ContentPage
+    {
+        public class Catfact
+        {
+            public int current_page { get; set; }
+            public IList<Facts> data { get; set; } 
+        }
+
+        public class Facts 
+        {
+            public string fact { get; set; }
+            public int length { get; set; }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            string json;
+            string url = "https://catfact.ninja/facts?max_length=100&limit=10";
+            using (var webClient = new WebClient())
+            {
+                json = webClient.DownloadString(url);
+            }
+            var response = JsonSerializer.Deserialize<Catfact>(json);
+            input.Text = response.data[0].fact.ToString(); 
+        }
+    }
+}
